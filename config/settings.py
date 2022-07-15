09198f0bf17e53f0a5dd4e0db562e23bc099e208
cha2hyun <cha2hyun.dev@gmail.com>
2022-07-15 12:24:10 +0900

    Blueprint : Url Setting

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Add Library
+    'rest_framework',
+    # Add App
+    'chat',
+    'cal'
 ]
 
 MIDDLEWARE = [
